4a7382fac5af94a3152598b9c52cc30753f1f303
Bontuko <bontubontu08@gmail.com>
2026-01-01 16:27:00 +0300

    Describe what you changed

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  define: {
-    "process.env": process.env, // optional: sometimes needed for some packages
-  },
   server: {
     // optional local dev config
     port: 5173,
