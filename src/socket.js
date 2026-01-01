c9a3f153836c383a56e8432be33f940c65516614
BoniDevSec <aberabontu776@gmail.com>
2026-01-01 15:18:47 +0300

    Connect to backend using environment variable
    Establish connection to backend using the VITE_API_BASE_URL environment variable.

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,7 @@
+// src/socket.js
 import { io } from "socket.io-client";
 
+// Connect to backend using environment variable
 const socket = io(import.meta.env.VITE_API_BASE_URL, {
   transports: ["websocket"],
 });
