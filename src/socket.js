0839238faa0f17b96b4db0155adcc6cf3e54947d
BoniDevSec <aberabontu776@gmail.com>
2026-01-01 15:31:58 +0300

    Refactor socket connection initialization

--- src/socket.js
+++ src/socket.js
@@ -1,9 +1,6 @@
 // src/socket.js
 import { io } from "socket.io-client";
 
-// Connect to backend using environment variable
-const socket = io(import.meta.env.VITE_API_BASE_URL, {
-  transports: ["websocket"],
-});
+const socket = io(import.meta.env.VITE_API_BASE_URL, { transports: ["websocket"] });
 
 export default socket;
